f2951940b7867e4f751a41e6af6ebd1fb3fdb465
JuHyeonKim07 <rlawngus000407@naver.com>
2019-07-12 00:21:35 +0900

    Todo part 2

--- todo.js
+++ todo.js
@@ -4,16 +4,37 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function deleteToDo(event){
+
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));  // localStorage 에 setItem 로 key 값이 TODOS_LS(toDos) 인 것에 value(toDos[]) 을 저장함.
+}
+
 function paintToDo(text){
    const li = document.createElement("li"); //li 라는 태그를 만드는 자바스크립트
    const delBtn = document.createElement("button");
+   const newId = toDos.length + 1;
    delBtn.value = "❌"
+   delBtn.addEventListener("click",deleteToDo);
    const span = document.createElement("span");
    span.innerText = text;
-   li.appendChild(span);
    li.appendChild(delBtn);
+   li.appendChild(span);
+   li.id = newId;
    toDoList.appendChild(li);
-}
+
+   const toDoObj = {
+       text : text,
+       id : newId
+   }
+
+   toDos.push(toDoObj)
+   saveToDos();
+}   
 
 function handleSubmit(event){
     event.preventDefault();
@@ -23,9 +44,12 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS); //저장한 localStorage 에 있는 key 값을 불러옴.
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);    //String 데이터를 Object 로 변경 하는 JSON.parse 적용.
+        parsedToDos.forEach(function(toDo){ //item 즉 parsedToDos 의 값이 toDo 에 담김
+            paintToDo(toDo.text);  //해당 item 의 text 값을 가져옴.
+        })
     }
 }
 
